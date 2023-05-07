6140b219222a16cf3f1d9eeb4404609044a47b3b
Steven Bougon <steven@bougon.com>
2023-05-07 13:46:32 -0700

    Update browsers.sh
    Removed firefox*, Safara tech preview and Chromium

--- src/os/installs/macos/browsers.sh
+++ src/os/installs/macos/browsers.sh
@@ -14,15 +14,15 @@ printf "\n"
 
 brew_install "Chrome" "google-chrome" "--cask"
 brew_install "Chrome Canary" "google-chrome-canary" "--cask" "homebrew/cask-versions"
-brew_install "Chromium" "chromium" "--cask" "homebrew/cask-versions"
+# brew_install "Chromium" "chromium" "--cask" "homebrew/cask-versions"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 printf "\n"
 
-brew_install "Firefox" "firefox" "--cask"
-brew_install "Firefox Developer" "firefox-developer-edition" "--cask" "homebrew/cask-versions"
-brew_install "Firefox Nightly" "firefox-nightly" "--cask" "homebrew/cask-versions"
+# brew_install "Firefox" "firefox" "--cask"
+# brew_install "Firefox Developer" "firefox-developer-edition" "--cask" "homebrew/cask-versions"
+# brew_install "Firefox Nightly" "firefox-nightly" "--cask" "homebrew/cask-versions"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -30,6 +30,6 @@ brew_install "Firefox Nightly" "firefox-nightly" "--cask" "homebrew/cask-version
 # https://github.com/alrra/dotfiles/issues
 
 if is_supported_version "$(get_os_version)" "10.11.4"; then
-    printf "\n"
-    brew_install "Safari Technology Preview" "safari-technology-preview" "--cask" "homebrew/cask-versions"
+#    printf "\n"
+#     brew_install "Safari Technology Preview" "safari-technology-preview" "--cask" "homebrew/cask-versions"
 fi
