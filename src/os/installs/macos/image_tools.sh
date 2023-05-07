8a12040f41413dfb08dbc4a0663a2502f85c5b6d
Steven Bougon <steven@bougon.com>
2023-05-07 13:47:20 -0700

    Update image_tools.sh
    removed gimp and image optimizer

--- src/os/installs/macos/image_tools.sh
+++ src/os/installs/macos/image_tools.sh
@@ -6,8 +6,8 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-print_in_purple "\n   Image Tools\n\n"
+# print_in_purple "\n   Image Tools\n\n"
 
-brew_install "GIMP" "gimp" "--cask"
-brew_install "ImageMagick" "imagemagick"
-brew_install "ImageOptim" "imageoptim" "--cask"
+# brew_install "GIMP" "gimp" "--cask"
+# brew_install "ImageMagick" "imagemagick"
+# brew_install "ImageOptim" "imageoptim" "--cask"
